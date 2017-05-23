96d3192edfbced2683a074d9376a5fc4cea7eba8
heathu <876734392@qq.com>
2017-05-24 02:08:12 +0800

    修改bug更多不显示2

--- nowstagram/views.py
+++ nowstagram/views.py
@@ -1,16 +1,42 @@
-# -*- encoding=utf-8 -*-
+# -*- encoding=UTF-8 -*-
 
-from nowstagram import app,db
-from flask import  render_template,redirect,request,flash,get_flashed_messages
-from flask_login import login_user, logout_user, login_required, current_user
-from models import Image,User,Comment
-import random,hashlib
-import json
+from nowstagram import app, db
+from models import Image, User, Comment
+from flask import render_template, redirect, request, flash, get_flashed_messages, send_from_directory
+import random, hashlib, json, uuid, os
+from flask_login import login_user, logout_user, current_user, login_required
+
+
+
+@app.route('/index/images/<int:page>/<int:per_page>/')
+def index_images(page, per_page):
+    paginate = Image.query.order_by(db.desc(Image.id)).paginate(page=page, per_page=per_page, error_out=False)
+    map = {'has_next': paginate.has_next}
+    images = []
+    for image in paginate.items:
+        comments = []
+        for i in range(0, min(2, len(image.comments))):
+            comment = image.comments[i]
+            comments.append({'username':comment.user.username,
+                             'user_id':comment.user_id,
+                             'content':comment.content})
+        imgvo = {'id': image.id,
+                 'url': image.url,
+                 'comment_count': len(image.comments),
+                 'user_id': image.user_id,
+                 'head_url':image.user.head_url,
+                 'created_date':str(image.created_date),
+                 'comments':comments}
+        images.append(imgvo)
+
+    map['images'] = images
+    return json.dumps(map)
 
 @app.route('/')
 def index():
-    images = Image.query.order_by('id desc').limit(10).all()
-    return render_template('index.html',images = images)
+    images = Image.query.order_by(db.desc(Image.id)).limit(10).all()
+    return render_template('index.html', images=images)
+
 
 @app.route('/image/<int:image_id>/')
 @login_required
@@ -18,7 +44,9 @@ def image(image_id):
     image = Image.query.get(image_id)
     if image == None:
         return redirect('/')
-    return render_template('pageDetail.html', image = image)
+    comments = Comment.query.filter_by(image_id=image_id).order_by(db.desc(Comment.id)).limit(20).all()
+    return render_template('pageDetail.html', image=image, comments=comments)
+
 
 @app.route('/profile/<int:user_id>/')
 @login_required
@@ -26,94 +54,102 @@ def profile(user_id):
     user = User.query.get(user_id)
     if user == None:
         return redirect('/')
-    paginate = Image.query.paginate(page=1, per_page=3)
-    return render_template('profile.html', user = user, has_next=paginate.has_next, images=paginate.items)
+    paginate = Image.query.filter_by(user_id=user_id).order_by(db.desc(Image.id)).paginate(page=1, per_page=3, error_out=False)
+    return render_template('profile.html', user=user, images=paginate.items, has_next=paginate.has_next)
+
 
 @app.route('/profile/images/<int:user_id>/<int:page>/<int:per_page>/')
 def user_images(user_id, page, per_page):
-    # 参数检查
-    paginate = Image.query.filter_by(user_id = user_id).paginate(page=page, per_page=per_page)
-
-    map = {'has_next':paginate.has_next}
+    paginate = Image.query.filter_by(user_id=user_id).order_by(db.desc(Image.id)).paginate(page=page, per_page=per_page, error_out=False)
+    map = {'has_next': paginate.has_next}
     images = []
     for image in paginate.items:
-        imgvo = {'id':image.id, 'url':image.url, 'comment_count': len(image.comments)}
+        imgvo = {'id': image.id, 'url': image.url, 'comment_count': len(image.comments)}
         images.append(imgvo)
+
     map['images'] = images
     return json.dumps(map)
 
 
 @app.route('/regloginpage/')
-def regloginpage(msg=''):
-    if current_user.is_authenticated:
-        return redirect('/')
-
+def regloginpage():
+    msg = ''
     for m in get_flashed_messages(with_categories=False, category_filter=['reglogin']):
         msg = msg + m
-    # 如果已经登录的就跳到首页
     return render_template('login.html', msg=msg, next=request.values.get('next'))
+
 
 def redirect_with_msg(target, msg, category):
     if msg != None:
         flash(msg, category=category)
     return redirect(target)
 
-@app.route('/login/', methods={'get', 'post'})
+
+@app.route('/login/', methods={'post', 'get'})
 def login():
     username = request.values.get('username').strip()
     password = request.values.get('password').strip()
-    # 校验
-    user = User.query.filter_by(username=username).first()
+
     if username == '' or password == '':
-        return redirect_with_msg('/regloginpage', u'用户名和密码不能为空', 'reglogin')
+        return redirect_with_msg('/regloginpage/', u'用户名或密码不能为空', 'reglogin')
 
     user = User.query.filter_by(username=username).first()
     if user == None:
-        return redirect_with_msg('/regloginpage', u'用户名不存在', 'reglogin')
+        return redirect_with_msg('/regloginpage/', u'用户名不存在', 'reglogin')
 
     m = hashlib.md5()
     m.update(password + user.salt)
-    if m.hexdigest() != user.password:
-        return redirect_with_msg('/regloginpage', u'密码错误', 'reglogin')
+    if (m.hexdigest() != user.password):
+        return redirect_with_msg('/regloginpage/', u'密码错误', 'reglogin')
 
     login_user(user)
 
     next = request.values.get('next')
-    if next != None and next.startswith('/') > 0:
+    if next != None and next.startswith('/'):
         return redirect(next)
+
     return redirect('/')
 
-@app.route('/reg/', methods={'get', 'post'})
+
+@app.route('/reg/', methods={'post', 'get'})
 def reg():
-    # args url里的
-    # form body里的
-    # values 全部
+    # request.args
+    # request.form
     username = request.values.get('username').strip()
     password = request.values.get('password').strip()
-    # 校验
-    user = User.query.filter_by(username = username).first()
-    if username == '' or password == '':
-        return redirect_with_msg('/regloginpage', u'用户名和密码不能为空', 'reglogin')
-    if user != None:
-        return redirect_with_msg('/regloginpage', u'用户名已存在', 'reglogin')
 
-    salt = ''.join(random.sample('0123456789abcdefghigklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 10))
+    if username == '' or password == '':
+        return redirect_with_msg('/regloginpage/', u'用户名或密码不能为空', 'reglogin')
+
+    user = User.query.filter_by(username=username).first()
+    if user != None:
+        return redirect_with_msg('/regloginpage/', u'用户名已经存在', 'reglogin')
+
+    # 更多判断
+
+    salt = '.'.join(random.sample('01234567890abcdefghigABCDEFGHI', 10))
     m = hashlib.md5()
-    m.update(password+salt)
+    m.update(password + salt)
     password = m.hexdigest()
     user = User(username, password, salt)
     db.session.add(user)
     db.session.commit()
+
     login_user(user)
 
     next = request.values.get('next')
-    if next != None and next.startswith('/') > 0:
+    if next != None and next.startswith('/'):
         return redirect(next)
+
     return redirect('/')
 
-    # 请大家考虑更多边界问题
 
-@app.route('/logout')
+@app.route('/logout/')
 def logout():
     logout_user()
     return redirect('/')
+
+
+
+
+
