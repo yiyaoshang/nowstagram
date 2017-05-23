5a83fad63c22abcaa97c3a1506e683d0df68b681
heathu <876734392@qq.com>
2017-05-23 18:27:45 +0800

    nowstagram项目课实现主页内容

--- nowstagram/views.py
+++ nowstagram/views.py
@@ -1,7 +1,7 @@
 # -*- encoding=utf-8 -*-
 
 from nowstagram import app
-from flask import  render_template
+from flask import  render_template,redirect
 from models import Image,User,Comment
 
 
@@ -9,3 +9,18 @@ from models import Image,User,Comment
 def index():
     images = Image.query.order_by('id desc').limit(10).all()
     return render_template('index.html',images = images)
+
+@app.route('/image/<int:image_id>/')
+def image(image_id):
+    image = Image.query.get(image_id)
+    if image == None:
+        return  redirect('/')
+    return render_template('pageDetail.html', image = image)
+
+
+@app.route('/profile/<int:user_id>/')
+def profile(user_id):
+    user = User.query.get(user_id)
+    if user == None:
+        return  redirect('/')
+    return render_template('profile.html', user = user)
